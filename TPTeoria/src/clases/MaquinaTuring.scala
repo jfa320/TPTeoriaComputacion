c05b49d52fd50a64cb7902b5ee73fe129dbdda5e
Fabi <Fabi@Fabi-PC>
2019-05-23 00:04:52 -0300

    Maquina funcionando (aparentemente) pero no escribe la cinta
    Resta arreglar que al procesar escriba la cinta y que emita algun error
    cuando no llega a un estado final

--- TPTeoria/src/clases/MaquinaTuring.scala
+++ TPTeoria/src/clases/MaquinaTuring.scala
@@ -1,31 +1,36 @@
 package clases
 
-final class MaquinaTuring(val transiciones: Map[(Estado, Char), (Estado, Char, Direccion)]){
+ class MaquinaTuring(val transiciones: Map[(Estado, Char), (Estado, Char, Direccion)]){
   
   def procesar(inicio: (Estado, Cinta)): (Estado, Cinta)={//B=Blanco (podria cambiarse)
     
-    def generarCintaNueva(cinta:Cinta,charNuevo:Char):List[Char]={
+    def generarListaCintaNueva(cinta:Cinta,charNuevo:Char):List[Char]={
+      @annotation.tailrec
+      def loop(listaRecursiva:List[Char],posicionActual:Integer):List[Char]={
+       
+        if(posicionActual<cinta.listaDeChar.length){
+          if(posicionActual==cinta.posActual){
+            loop(charNuevo::listaRecursiva,posicionActual+1)
+          }
+          else{
+            val lista=listaRecursiva;
+            loop(cinta.listaDeChar.apply(posicionActual)::lista,posicionActual+1)
+          }
+        }
+        else{
+          listaRecursiva
+        }
+      }
       
-      
-//      def loop(listaRecursiva:List[Char],posicion:Integer):List[Char]={
-//        val listaDeChar=List[Char]
-//        if(posicion==cinta.posActual){
-//          loop()
-//        }
-//        else{
-//          
-//        }
-//      }
-//      
-      List('a') //cambiar esto,solo hecho para no tener error de no devolver nada
+      loop(List(),0)
     }
     
     
     def moviendoCinta(estadoActual:Estado,cinta:Cinta):(Estado,Cinta)={ //con este metodo simular el movimiento
-      val charActual=cinta.listaDeChar(cinta.posActual) 
+      val charActual=cinta.listaDeChar.apply(cinta.posActual) 
       val tuplaDestino=transiciones(estadoActual,charActual)
       
-      val cintaNueva=new Cinta(generarCintaNueva(cinta,tuplaDestino._2),if (tuplaDestino._3.dir=='R') cinta.posActual+1 else cinta.posActual-1)
+      val cintaNueva=new Cinta(generarListaCintaNueva(cinta,tuplaDestino._2),if (tuplaDestino._3.dir=='R') cinta.posActual+1 else cinta.posActual-1)
       (tuplaDestino._1,cintaNueva)
     }
     
--- TPTeoria/src/clases/Pruebas.scala
+++ TPTeoria/src/clases/Pruebas.scala
@@ -2,20 +2,26 @@ package clases
 
 object Pruebas {
   def main(Args:Array [String]):Unit={
-    val transicion= Map((new Estado("q"), '0')-> (new Estado("q"), '1', new Direccion('R')), //pruebo con MT de ejemplo
-                        (new Estado("q"), '1')-> (new Estado("f"), '0', new Direccion('R')),//de las diapos
-                        (new Estado("q"), 'B')-> (new Estado("q"), '1', new Direccion('L')))
+    
+    val estado=new Estado("q")
+    val estadof=new Estado("f")
+    
+    val transiciones= Map.apply((estado, '0')-> (estado, '1', new Direccion('R')), //pruebo con MT de ejemplo
+                        (estado, '1')-> (estadof, '0', new Direccion('R')),//de las diapos
+                        (estado, 'B')-> (estado, '1', new Direccion('L')))
                         
-    val maquina=new MaquinaTuring(transicion)
+    val maquina=new MaquinaTuring(transiciones)
     
     val lista=List('B', 'B', '0', '0','B','B')
     
-    val cinta=new Cinta(lista,2)
+//    val lista2='a'::lista//agregar lista
+    
+    
+    
+   val cinta=new Cinta(lista,2)
    
-    
-    
-    maquina.transiciones.get(new Estado("q"),'1')
-    
-    println(maquina.procesar(new Estado("q"),cinta))
+//   println( transiciones(new Estado("q"),'0'))
+    val tuplaFinal=maquina.procesar(estado,cinta)
+    println(tuplaFinal._1.nombre,"(" + tuplaFinal._2.listaDeChar+","+tuplaFinal._2.posActual+")")
   }
 }
